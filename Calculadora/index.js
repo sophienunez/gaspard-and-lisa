c2e8f1585663e99b6fb5c364a28b020ecc03b263
sophienunez <98788221+sophienunez@users.noreply.github.com>
2022-03-04 01:54:36 -0500

    Add files via upload

---
+++ Calculadora/index.js
@@ -0,0 +1,14 @@
+const displayValorArriba = document.getElementById('valorarriba');
+const displayValorAbajo = document.getElementById('valorabajo');
+const botonesNumeros = document.querySelectorAll('.number');
+const botonesOperadores = document.querySelectorAll('.operator');
+
+const display = new Display(displayValorArriba, displayValorAbajo);
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value))
+});
